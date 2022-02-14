ba648a0ea26a3a455b1395990f3b356e6850613d
Wellington126 <wellington.antonio@academico.ifpb.edu.br>
2022-02-14 17:53:36 -0300

    commit

--- src/Componentes/areaPostagem/AreaPostagem.js
+++ src/Componentes/areaPostagem/AreaPostagem.js
@@ -7,7 +7,7 @@ export default function AreaPostagem(){
 
      let postagem1 = {
                       "titulo": "Associção Mulheres Águias",
-                       "descricao": "Desde 2014, essas mulheres vem trabalhando na perspectivada economia solidária, com o assessoramento da incubadora tecnológica de empreendimentos solidários do IFPB (INCUTES/IFPB). Sua primeira produção se desenvolveu com a confecção de bolsas reutilizando banners, que teve oportunidade de lançamento no III fórum mundial de educação", 
+                       "descricao": "Desde 2014, essas mulheres vem trabalhando na perspectiva da economia solidária, com o assessoramento da incubadora tecnológica de empreendimentos solidários do IFPB (INCUTES/IFPB). Sua primeira produção se desenvolveu com a confecção de bolsas reutilizando banners, que teve oportunidade de lançamento no III fórum mundial de educação", 
                        "foto": fotoPost1}
 
      return(
